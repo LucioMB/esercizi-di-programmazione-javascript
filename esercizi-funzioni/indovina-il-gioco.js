4cf48e269d83b2c6b1973210e4fd19a200fc1bec
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-07-20 16:23:33 +0200

    Update indovina-il-gioco.js

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -14,3 +14,10 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function day(x) {
+  let a = x >= 1 && x <= 7 ? true : false;
+  let b = a === true && x === 1 ? 'Lunedì' : a === true && x === 2 ? 'Martedì' :  a === true && x === 3 ? 'Mercoledì' : a === true && x === 4 ? 'Giovedì' : a === true && x === 5 ? 'Venerdì' : a === true && x === 6 ? 'Sabato' :  a === true && x === 7 ? 'Domenica' : 'Peccato non posso indovinare il giorno';
+  console.log(b)
+}
+day(2)
