6e3414e9085f6f099dbe7ae8d4f31ecfa9de7eb3
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-07-24 12:34:37 +0200

    Update caldo-o-freddo.js

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,11 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function temp(gradi) {
+  let a = gradi.sort((x, y) => x - y);
+  let max = a.pop();
+  let min = a.shift();
+  console.log(`La giornata più calda ha registrato ${max} gradi, quella più fredda ${min} gradi`)
+}
+temp([a = 10, b = -2, c = 31, d = 22, e = 15, f = -6, g = 7])
