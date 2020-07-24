37acec6e7e800bb5d40986b21105ec1e3024599f
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-07-24 16:27:53 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,22 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function voto(v) {
+  if (v < 18) {
+    console.log('Insufficiente');
+  } else if (v < 21){
+    console.log('Sufficiente');    
+	} else if (v < 24) {
+    console.log('Buono');
+  } else if (v < 27) {
+    console.log('Distinto');
+  } else if (v <= 29) {
+    console.log('Ottimo');
+  } else if (v == 30) {
+    console.log('Eccellente');
+  } else {
+    console.log('Voto non valido!!');
+  }
+}
+voto(21)
