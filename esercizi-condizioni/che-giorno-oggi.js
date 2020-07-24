3422c029be6d70780953a15b3e7d5c19f21cf07b
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-07-24 13:38:10 +0200

    Update che-giorno-oggi.js

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -43,3 +43,37 @@ function day(x) {
 }
 }
 day(6)
+
+
+//Variante mesi
+
+function day(x) {
+  switch (x) {
+    case 1: console.log('Gennaio');
+      break;
+    case 2: console.log('Febbraio');
+      break;
+    case 3: console.log('Marzo');
+      break;
+    case 4: console.log('Aprile');
+      break;
+    case 5: console.log('Maggio');
+      break;
+    case 6: console.log('Giugno');
+      break;
+    case 7: console.log('Luglio');
+      break;
+    case 8: console.log('Agosto');
+      break;
+    case 9: console.log('Settembre');
+      break;
+    case 10: console.log('Ottobre');
+      break;
+    case 11: console.log('Novembre');
+      break;
+    case 12: console.log('Dicembre');
+      break;
+    default: console.log('Errore! Mese dell\'anno non valido!')
+}
+}
+day(2)
