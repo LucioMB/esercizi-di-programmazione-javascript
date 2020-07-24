ea45bedf768da6ccc84b96aff3e42bd1f527193f
LucioMB <57898810+LucioMB@users.noreply.github.com>
2020-07-24 13:33:52 +0200

    Update che-giorno-oggi.js

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -22,3 +22,24 @@
 
   http://www.imparareaprogrammare.it
 */
+
+function day(x) {
+  switch (x) {
+    case 1: console.log('Lunedì');
+      break;
+    case 2: console.log('Martedì');
+      break;
+    case 3: console.log('Mercoledì');
+      break;
+    case 4: console.log('Giovedì');
+      break;
+    case 5: console.log('Venerdì');
+      break;
+    case 6: console.log('Sabato');
+      break;
+    case 7: console.log('Domenica');
+      break;
+    default: console.log('Errore! Giorno della settimana non valido!')
+}
+}
+day(6)
